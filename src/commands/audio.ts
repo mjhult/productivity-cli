7a6ec0ee7451b547e64181321fecf69201154017
Wvaviator <wvaviator@gmail.com>
2022-09-25 11:06:56 -0500

    add code to create prompt for switching audio devices

---
+++ src/commands/audio.ts
@@ -0,0 +1,40 @@
+import inquirer from 'inquirer';
+import {
+  getOutputDevices,
+  getInputDevices,
+  setDefaultOutputDevice,
+  setDefaultInputDevice,
+} from 'macos-audio-devices';
+
+export const selectAudio = async () => {
+  const [outputDevices, inputDevices] = await Promise.all([
+    getOutputDevices(),
+    getInputDevices(),
+  ]);
+
+  const audioDevicePrompt = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'output',
+      choices: outputDevices.map((device) => ({
+        name: device.name,
+        value: device.id,
+      })),
+      message: 'Select an output device:',
+    },
+    {
+      type: 'list',
+      name: 'input',
+      choices: inputDevices.map((device) => ({
+        name: device.name,
+        value: device.id,
+      })),
+      message: 'Select an input device:',
+    },
+  ]);
+
+  await Promise.all([
+    setDefaultOutputDevice(audioDevicePrompt.output),
+    setDefaultInputDevice(audioDevicePrompt.input),
+  ]);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
-import { gotoGithubRepo } from './commands/github';
-import { executeGoogleSearch } from './commands/google';
-import { executeYoutubeSearch } from './commands/youtube';
+import { selectAudio } from './commands/audio.js';
+import { gotoGithubRepo } from './commands/github.js';
+import { executeGoogleSearch } from './commands/google.js';
+import { executeYoutubeSearch } from './commands/youtube.js';
 
 const [_nodeExecutablePath, executablePath, ...args] = process.argv;
 const pathComponents = executablePath.split('/');
@@ -11,6 +12,7 @@ const commandActions = {
   google: () => executeGoogleSearch(args),
   youtube: () => executeYoutubeSearch(args),
   github: () => gotoGithubRepo(args),
+  audio: () => selectAudio(),
 };
 
 commandActions[command]();
