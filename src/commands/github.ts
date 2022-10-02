eccccbef8d5329262a2910265496133d287bb716
Mathew Hultquisr <mathew@gates.services>
2022-10-02 10:47:23 -0400

    Added check for empty github handle.

--- src/commands/github.ts
+++ src/commands/github.ts
@@ -2,7 +2,12 @@ import navigate from '../utils/navigate';
 import { ConfigUtils } from '../utils/config';
 
 export const gotoGithubRepo = (repoArgs: string[]) => {
-  let url = `https://github.com/${ConfigUtils.getConfig().githubHandle}/`;
+
+  const config = ConfigUtils.getConfig();
+
+  if (!config.githubHandle) return console.log('Please run \'pconfig set githubHandle <handle>\' before running this command.');
+
+  let url = `https://github.com/${config.githubHandle}/`;
 
   if (repoArgs.length) {
     url += repoArgs[0];
